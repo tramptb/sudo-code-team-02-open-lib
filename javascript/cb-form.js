0cd9a7aba0c80bac76034209667fc861b7f8a722
Suxi <91678688+suxiii@users.noreply.github.com>
2023-12-22 16:32:48 +0700

    Refactor form validation and add success/error indicators

--- javascript/cb-form.js
+++ javascript/cb-form.js
@@ -1,70 +1,113 @@
-window.onload = function () {
-  // validate form data
+// validation for form
 
-  let id = (id) => document.getElementById(id);
+const contributeForm = document.querySelector("#contributeForm");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const category = document.querySelector("#category");
+const bookFile = document.querySelector("#bookFile");
+const contributorName = document.querySelector("#contributorName");
+const contributorEmail = document.querySelector("#contributorEmail");
 
-  let classes = (classes) => document.getElementsByClassName(classes);
 
-  let title = id("title"),
-    author = id("author"),
-    category = id("category"),
-    bookFile = id("bookFile"),
-    contributorName = id("contributorName"),
-    contributorEmail = id("contributorEmail"),
-    formGroup = id("form-group"),
-    agreement = classes("form-check-input");
-  (errorMsg = classes("error")),
-    (successIcon = classes("success-icon")),
-    (failureIcon = classes("failure-icon"));
+// validation for register form
+document.addEventListener("DOMContentLoaded", function () {
+  const contributeForm = document.querySelector("#contributeForm");
 
-  // ...
-
-  let contributeForm = id("contributeForm"); // Corrected ID here
-
-  contributeForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    engine(title, 0, "Title cannot be blank");
-    engine(author, 1, "Author cannot be blank");
-    engine(category, 2, "Category cannot be blank");
-    engine(bookFile, 3, "Book File cannot be blank");
-    engine(contributorName, 4, "Contributor Name cannot be blank");
-    engine(contributorEmail, 5, "Contributor Email cannot be blank");
-
-    // Use engine for the agreement checkbox validation
-    engine(agreement[0], 6, "Please agree to our terms and conditions");
-
-    // Check if all validations passed before displaying success message
-    let allValid = false;
-    for (let i = 0; i < errorMsg.length; i++) {
-      if (errorMsg[i].innerHTML !== "") {
-        allValid = false;
-        break;
-      } else {
-        allValid = true;
-      }
-    }
-
-    if (allValid) {
-      formGroup.innerHTML = `<h2 class="form-message">Thank you for contributing to our library</h2>`;
-    } else {
-      return false;
-    }
+  contributeForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    validateInput();
   });
+});
 
-  // engine function which is going to validate form data
+function setError(input, message) {
+  const formGroup = input.parentElement;
+  const errorDisplay = formGroup.querySelector(".error");
 
-  let engine = (id, serial, message) => {
-    if (id.value.trim() === "") {
-      errorMsg[serial].innerHTML = message;
-      failureIcon[serial].style.opacity = "1";
-      successIcon[serial].style.opacity = "0";
-      id.style.border = "2px solid red";
-    } else {
-      errorMsg[serial].innerHTML = "";
-      failureIcon[serial].style.opacity = "0";
-      successIcon[serial].style.opacity = "1";
-      id.style.border = "2px solid green";
-    }
-  }; 
+  errorDisplay.innerText = message;
+  formGroup.classList.add("error");
+  formGroup.classList.remove("error");
+}
+
+const setSuccess = (input) => {
+  const formGroup = input.parentElement;
+  const errorDisplay = formGroup.querySelector(" .error");
+
+  if (errorDisplay) {
+    errorDisplay.innerText = "";
+  }
+
+  formGroup.classList.add("success");
+  formGroup.classList.remove("error");
 };
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(String(email).toLowerCase());
+};
+
+// validate checkbox agreement
+// function validateCheckbox() {
+//   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+//   let checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
+//   if (!checkedOne) {
+//     alert("Please agree to the terms and conditions");
+//   }
+// }
+
+function validateInput() {
+  const titleValue = title.value.trim();
+  const authorValue = author.value.trim();
+  const categoryValue = category.value.trim();
+  const bookFileValue = bookFile.value.trim();
+  const nameValue = contributorName.value.trim();
+  const emailValue = contributorEmail.value.trim();
+//   const checkboxValue = checkbox.value.trim();
+
+
+  if (titleValue === "") {
+    setError(title, "Title cannot be blank");
+  } else {
+    setSuccess(title);
+  }
+
+  if (authorValue === "") {
+    setError(author, "Author cannot be blank");
+  } else {
+    setSuccess(author);
+  }
+
+  if (categoryValue === "") {
+    setError(category, "Category cannot be blank");
+  } else {
+    setSuccess(category);
+  }
+
+  if (bookFileValue === "") {
+    setError(bookFile, "Book File cannot be blank");
+  } else {
+    setSuccess(bookFile);
+  }
+
+  if (nameValue === "") {
+    setError(contributorName, "Name cannot be blank");
+  } else {
+    setSuccess(contributorName);
+  }
+
+  if (emailValue === "") {
+    setError(contributorEmail, "Email cannot be blank");
+  } else if (!isValidEmail(emailValue)) {
+    setError(contributorEmail, "Email is not valid");
+  } else {
+    setSuccess(contributorEmail);
+  }
+
+//   if (checkboxValue === "") {
+//     setError(checkbox, "Please agree to the terms and conditions");
+//   } else {
+//     setSuccess(checkbox);
+//   }
+
+
+}
